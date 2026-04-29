15ecf3aea44f2d59c09ef6f40622d755a504a996
mrkeylost <nikosam08@gmail.com>
2026-04-29 22:06:41 +0700

    deploy: prepare for vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import UserRouter from "./routes/auth.route.js";
 import MessageRouter from "./routes/message.route.js";
 import AppError from "./utils/AppError.js";
@@ -12,6 +13,7 @@ if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
 
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.urlencoded({ extended: true }));
@@ -23,6 +25,14 @@ if (process.env.NODE_ENV === "development") {
 
 app.use("/api/auth", UserRouter);
 app.use("/api/message", MessageRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.use("*", (req, res, next) => {
   next(new AppError("Page Not Found", 404));
--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -3,7 +3,10 @@ import api from "../utils/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "/_/backend/api";
 
 export const useAuthStore = create((set, get) => ({
   auth: null,
